c7ac8e197a1ed74961f9fb76a36e6cd225e316fc
WilliamNeves123 <100882475+WilliamNeves123@users.noreply.github.com>
2022-03-03 20:02:55 -0300

    Add files via upload

---
+++ C.c
@@ -0,0 +1,24 @@
+void main{
+char grade = 'C';
+switch( grade) {
+case 'A' :
+printf("Excelente!");
+break;
+case 'B' :
+case 'C' :
+printf("Bem feito!");
+break;
+case 'D' :
+printf("Você passou!");
+break;
+case 'E' :
+printf("Melhor tentar novamente!");
+break;
+default :
+printf("Conceito inválido!");
+}
+
+
+
+
+}
